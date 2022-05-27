a0544637b669d433ecabf820e96d7a3a77aaf78d
antonraguzin <raguzin82@gmail.com>
2022-05-27 17:16:41 +0300

    Код написан, калькулятор считает верно

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,3 +1,10 @@
 public class CreditPaymentService {
-
+    public double calculate(int amount, int period, double percent) {
+        double i = percent / 12 / 100; // сумма процентов в месяц
+        int n = period * 12; // срок кредита в месяцах
+        double pow = Math.pow(1 + i, n); // формула возведения в степень выражения
+        double k = i * pow / (pow - 1); // коэфициент аннуитета
+        double payment = k * amount; // расчет ежемесячного платежа
+        return payment;
+        }
 }
--- src/Maim.java
+++ src/Maim.java
@@ -1,9 +1,10 @@
 public class Maim {
     public static void main(String[] args) {
-
-        int amount = 1_000_000;
-        int period = 12;
-        double percent = 9.99;
-
-    }
+        CreditPaymentService service = new CreditPaymentService();
+        int amount = 1_000_000; // сумма кредита в рублях
+        int period = 1; // срок кредита в годах
+        double percent = 9.99; // ставка в процентах годовых
+        double payment = service.calculate(amount, period, percent);
+        System.out.printf("Ежемесячный платеж - " + "%.0f", payment);
+        }
 }
